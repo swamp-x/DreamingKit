4c1566d9ad289ed43cd78800402825d17068bf45
lxw086 <lxw086@gmail.com>
2014-09-25 22:08:19 +0800

    hello DreamingKit

---
+++ DreamingKit/DreamingKit/DreamingObject/DreamingObject.h
@@ -0,0 +1,13 @@
+//
+//  DreamingObject.h
+//  DreamingKit
+//
+//  Created by lxw086 on 14-9-25.
+//  Copyright (c) 2014年 swampx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DreamingObject : NSObject
+
+@end
---
+++ DreamingKit/DreamingKit/DreamingObject/DreamingObject.m
@@ -0,0 +1,13 @@
+//
+//  DreamingObject.m
+//  DreamingKit
+//
+//  Created by lxw086 on 14-9-25.
+//  Copyright (c) 2014年 swampx. All rights reserved.
+//
+
+#import "DreamingObject.h"
+
+@implementation DreamingObject
+
+@end
